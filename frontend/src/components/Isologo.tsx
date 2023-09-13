4f9f7fde2f25ec53a6e94567fcb78f3a2bb9eb87
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-13 17:17:34 +0200

    #2 isologo

--- frontend/src/components/Isologo.tsx
+++ frontend/src/components/Isologo.tsx
@@ -2,13 +2,10 @@ function Isologo () {
   return (
     <div className='pt-20 pb-8 md:pt-8 md:m-10 text-center md:text-left max-w-[400px] mx-auto'>
       <img
-        src='src/assets/isologo-f5.png'
+        src='src/assets/LogoFactoria.png'
         className='max-w-[300px] h-auto mb-10 ml-7'
         alt='isologo-f5'
       />
-      <h1 className='font-medium mt-4 text-FF4700 md:text-left mt-4'>
-        GESTIÓN DE CONVOCATORIAS Y SUBVENCIONES DE SOMOS F5
-      </h1>
     </div>
   )
 }
