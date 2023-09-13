3270576226892e9cec17db75d74e8b63d62f29b9
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-13 12:00:49 +0200

    #2 user home

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import MainNav from './ui/MainNav'
+import { Search } from './ui/search'
+import UserNav from './ui/user-nav'
+
+function Navbar () {
+  return (
+    <>
+      <MainNav />
+      <UserNav />
+      <Search />
+    </>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
+import Navbar from '../components/Navbar'
+
 function HomePage () {
   return (
-    <div>HomePage</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
