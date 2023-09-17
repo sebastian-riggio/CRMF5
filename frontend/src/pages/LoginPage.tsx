5f121aedbb2cf741001462d08eba932810e8440e
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-17 15:58:57 +0200

    #2 ruta entrar corregida

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,13 +4,13 @@ import { LoginTab } from '../components/LoginTab'
 function LoginPage() {
   return (
     <div>
-    <div className='min-h-screen md:flex justify-center items-center bg-gray-100'>
-      <div >
-        <Isologo />
-      </div>
-      <div className='my-8'>
-        <LoginTab />
-      </div>
+      <div className='min-h-screen md:flex justify-center items-center bg-gray-100'>
+        <div >
+          <Isologo />
+        </div>
+        <div className='my-8'>
+          <LoginTab />
+        </div>
       </div>
     </div>
   )
