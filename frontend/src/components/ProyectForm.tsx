8eb7dab50f728dc05db4d03996ec4ff8db91a30b
sebastian-riggio <sebastian.riggio@gmail.com>
2023-09-22 11:30:27 +0200

    agregar escuelas formulario proyectos

--- frontend/src/components/ProyectForm.tsx
+++ frontend/src/components/ProyectForm.tsx
@@ -44,6 +44,9 @@ const departamento = [
   { label: 'Factoría F5-Admin,contabilidad y finanzas', value: 'Factoría F5-Admin,contabilidad y finanza' },
   { label: 'Factoría F5-Oficina soporte proyectos y calidad', value: 'Factoría F5-Oficina soporte proyectos y calida' },
   { label: 'Factoría F5-Dirección general', value: 'Factoría F5-Dirección general' },
+  { label: 'Factoria F5 - Madrid', value: 'Factoria F5 - Madrid' },
+  { label: 'Factoria F5 - Asturias', value: 'Factoria F5 - Asturias' },
+  { label: 'Factoria F5 - Barcelona', value: 'Factoria F5 - Barcelona' },
   { label: 'Otro', value: 'Otro' }
 ] as const
 
