b4283532382be5edced807a20b46cff7773f70c6
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-24 19:09:34 +0200

    #2 login posicionado

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -5,10 +5,10 @@ function LoginPage () {
   return (
     <div>
       <div className='min-h-screen md:flex justify-center items-center bg-gray-100'>
-        <div>
+        <div className='md:mr-5'>
           <Isologo />
         </div>
-        <div className='my-8'>
+        <div className='md:ml-5'>
           <LoginTab />
         </div>
       </div>
