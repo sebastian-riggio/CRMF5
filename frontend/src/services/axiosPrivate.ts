368768e8e23b00d1f586d66a43131d9345bcc373
Arielparicio28 <arielaparicio100@gmail.com>
2023-09-25 22:42:29 +0200

    #8 done all projects page

--- frontend/src/services/axiosPrivate.ts
+++ frontend/src/services/axiosPrivate.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = 'http://localhost:3000/api/v1'
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
-});
+})
--- frontend/src/services/proyectos.ts
+++ frontend/src/services/proyectos.ts
@@ -1,7 +1,10 @@
-import { Projects } from "../interfaces/projects";
-import { axiosPrivate } from "./axiosPrivate";
+import { Projects } from '../interfaces/projects'
+import { axiosPrivate } from './axiosPrivate'
 
+export const createProject = (data:Projects) => {
+  return axiosPrivate.post('/projects/register', data)
+}
 
-  export const createProject = (data:Projects)=> {
-    return axiosPrivate.post(`/projects/register`,data)
-  }
+export const getProjects = () => {
+  return axiosPrivate.get('projects')
+}
