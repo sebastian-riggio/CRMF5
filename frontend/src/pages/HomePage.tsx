113ea1c6ef64d333bb725ccda76c449706f020b6
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-27 00:02:03 +0200

    #10 dashboard editado. Aun falta Calendario

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,30 +1,150 @@
-import { CalendarDashboard } from '../components/home/CalendarDashboard'
-import CardReview from '../components/home/CardReview'
+import GraphicData from '../components/home/GraphicData'
+import { RecentCloseProjects } from '../components/home/RecentCloseProjects'
 import { SideBar } from '../components/home/SideBar'
-import { Card } from '../components/ui/card'
-import { Separator } from '../components/ui/separator'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
+import { Tabs, TabsContent } from '../components/ui/tabs'
 
 function HomePage () {
   return (
-    <>
-      <Card className='container mx-auto md:mt-5 flex'>
-        <div className='hidden md:grid lg:grid-cols-5 flex'>
-          <div className='hidden lg:block'>
-            <SideBar />
-          </div>
-          <div className='lg:col-span-1 ml-3'>
-            <Separator orientation='vertical' />
-          </div>
-          <div>
-            <CalendarDashboard />
-          </div>
-          {/*           <div>
-            <CardReview />
-          </div> */}
-        </div>
-
-      </Card>
-    </>
+    <Card className='container mx-auto mt-5 flex px-4'>
+      <div className='w-1/5 pr-5'>
+        <SideBar />
+      </div>
+      <div className='w-4/5'>
+        <Tabs defaultValue='overview' className='space-y-4 my-4'>
+          <TabsContent value='overview' className='space-y-4'>
+            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Total de proyectos
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24' height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='h-5 w-5 text-muted-foreground'
+                  >
+                    <path d='m16 6 4 14' />
+                    <path d='M12 6v14' />
+                    <path d='M8 8v12' />
+                    <path d='M4 4v16' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>500</div>
+                  <p className='text-xs text-muted-foreground'>
+                    +20% que el último año
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Convocatorias
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
+                    <circle cx='9' cy='7' r='4' />
+                    <path d='M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>450</div>
+                  <p className='text-xs text-muted-foreground'>
+                    +10% que el último año
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>Total de proyectos financiados</CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <rect width='20' height='14' x='2' y='5' rx='2' />
+                    <path d='M2 10h20' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>424</div>
+                  <p className='text-xs text-muted-foreground'>
+                    +5% que el último mes
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>
+                    Proyectos en trámite
+                  </CardTitle>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    className='h-4 w-4 text-muted-foreground'
+                  >
+                    <path d='M22 12h-4l-3 9L9 3l-3 9H2' />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>50</div>
+                  <p className='text-xs text-muted-foreground'>
+                    +lorem ipzum
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
+              <Card className='col-span-4'>
+                <CardHeader>
+                  <CardTitle>Aqui va un gráfico</CardTitle>
+                </CardHeader>
+                <CardContent className='pl-2'>
+                  <GraphicData />
+                </CardContent>
+              </Card>
+              <Card className='col-span-3'>
+                <CardHeader>
+                  <CardTitle>Últimos proyectos 100% financiados</CardTitle>
+                  <CardDescription>
+                    Factoria F5 cerró 5 proyectos este mes
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <RecentCloseProjects />
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </Card>
   )
 }
 
