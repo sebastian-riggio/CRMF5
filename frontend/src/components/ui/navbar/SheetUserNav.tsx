582e8f5f14d3b5426e6573a1aeb456f3871fb63c
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-09-28 14:51:36 +0200

    #12 navbar

--- frontend/src/components/ui/navbar/SheetUserNav.tsx
+++ frontend/src/components/ui/navbar/SheetUserNav.tsx
@@ -15,7 +15,7 @@ export function SheetUserNav () {
     <Sheet>
       <SheetTrigger asChild>
         <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-          <Avatar className='h-8 w-8 md:h-10 md:w-10'>
+          <Avatar className='h-7 w-7 md:h-8 md:w-8'>
             <AvatarImage src='/avatars/01.png' alt='@shadcn' />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
