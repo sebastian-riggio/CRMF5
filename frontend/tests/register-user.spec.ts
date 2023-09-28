cbce9159f4b7a9e9f0bf3273615ff94d8a51255b
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-09-28 11:41:13 +0200

    #6 test register user

--- frontend/tests/register-user.spec.ts
+++ frontend/tests/register-user.spec.ts
@@ -5,71 +5,109 @@ test.beforeEach(async ({ page }) => {
 })
 
 test.describe('Register user page', () => {
-
   test('Display form title', async ({ page }) => {
-    const titleForm = page.getByRole('heading', { name: /nuevo usuario/i})
-  
-    await expect(titleForm).toBeVisible();
-    })
+    const titleForm = page.getByRole('heading', { name: /nuevo usuario/i })
+
+    await expect(titleForm).toBeVisible()
+  })
 
   test('Display sub title', async ({ page }) => {
-    const subtitle = page.getByText( /Completar los datos del nuevo usuario del Factoria F5/i)
+    const subtitle = page.getByText(/Completar los datos del nuevo usuario del Factoria F5/i)
 
     await expect(subtitle).toBeVisible()
   })
 })
 
 test.describe('Form register new user', () => {
-test('Name and lastname input', async ({ page }) => {
-  const nameLabel = page.getByLabel('Nombre y Apellido')
-  await expect(nameLabel).toBeVisible()
+  test('Name and lastname input', async ({ page }) => {
+    const nameLabel = page.getByLabel('Nombre y Apellido')
+    await expect(nameLabel).toBeVisible()
 
-  const nameInput = page.getByPlaceholder('Nombre y Apellido')
-  await expect(nameInput).toBeVisible()
- })
+    const nameInput = page.getByPlaceholder('Nombre y Apellido')
+    await expect(nameInput).toBeVisible()
+  })
 
- test('Department or management center input', async ({ page }) => {
-  const departamentLabel = page.getByText('Departamento o centro gestor')
-  await expect(departamentLabel).toBeVisible()
+  test('Name and lastname that can be written', async ({ page }) => {
+    await page.type('[data-testid=nombre-input]', 'John Doe')
 
-  const departamentInput = page.getByLabel(/Departamento o centro gestor/i)
-  await expect(departamentInput).toBeVisible()
-  await departamentInput.click()
- })
+    const inputValue = await page.getAttribute('[data-testid=nombre-input]', 'value')
+    await expect(inputValue).toBe('John Doe')
+  })
 
- test('Work area input', async ({ page }) => {
-  const workLabel = page.getByText('Área de trabajo en Factoria F5')
-  await expect(workLabel).toBeVisible()
+  test('Department or management center input', async ({ page }) => {
+    const departamentLabel = page.getByText('Departamento o centro gestor')
+    await expect(departamentLabel).toBeVisible()
 
-  const workInput = page.getByPlaceholder(/Área de trabajo/i)
-  await expect(workInput).toBeVisible()
-   })
+    const departamentInput = page.getByLabel(/Departamento o centro gestor/i)
+    await expect(departamentInput).toBeVisible()
+    await departamentInput.click()
+  })
 
-   test('Date input', async ({ page }) => {
+  test('Work area input', async ({ page }) => {
+    const workLabel = page.getByText('Área de trabajo en Factoria F5')
+    await expect(workLabel).toBeVisible()
+
+    const workInput = page.getByPlaceholder(/Área de trabajo/i)
+    await expect(workInput).toBeVisible()
+  })
+
+  test('Work area that can be written', async ({ page }) => {
+    await page.type('[data-testid=work-area-input]', 'Recursos humanos')
+
+    const inputValue = await page.getAttribute('[data-testid=work-area-input]', 'value')
+    await expect(inputValue).toBe('Recursos humanos')
+  })
+
+  test('Date input', async ({ page }) => {
     const dateLabel = page.getByText('Fecha inicio de actividad')
     await expect(dateLabel).toBeVisible()
-  
+
     const dateInput = page.getByLabel(/Fecha inicio de actividad/i)
     await expect(dateInput).toBeVisible()
     await dateInput.click()
-   })
+  })
 
-   test('Email input', async ({ page }) => {
+  test('Email input', async ({ page }) => {
     const emailLabel = page.getByText('Correo electrónico')
     await expect(emailLabel).toBeVisible()
-  
+
     const emailInput = page.getByPlaceholder(/Correo electrónico/i)
     await expect(emailInput).toBeVisible()
-     })
+  })
+
+  test('Email that can be written', async ({ page }) => {
+    await page.type('[data-testid=email-input]', 'johndoe@example.com')
+
+    const inputValue = await page.getAttribute('[data-testid=email-input]', 'value')
+    await expect(inputValue).toBe('johndoe@example.com')
+  })
 
   test('Phone input', async ({ page }) => {
     const phoneLabel = page.getByText('Teléfono de contacto')
     await expect(phoneLabel).toBeVisible()
-    
+
     const phoneInput = page.getByPlaceholder(/telefono/i)
     await expect(phoneInput).toBeVisible()
-     })
- })
+  })
 
+  test('Phone that can be written', async ({ page }) => {
+    await page.type('[data-testid=phone-input]', '123456789')
 
+    const inputValue = await page.getAttribute('[data-testid=phone-input]', 'value')
+    await expect(inputValue).toBe('123456789')
+  })
 
+  test('Create button', async ({ page }) => {
+    const button = await page.getByRole('button', { name: /Crear/i })
+    await expect(button).toBeVisible()
+    await button.click()
+    // await expect(page).toHaveURL('http://localhost:5173/home')
+  })
+
+  test('Cancel button', async ({ page }) => {
+    const button = await page.getByRole('button', { name: /Cancelar/i })
+    await expect(button).toBeVisible()
+    await button.click()
+    // await expect(page).toHaveURL('http://localhost:5173/home')
+  })
+})
