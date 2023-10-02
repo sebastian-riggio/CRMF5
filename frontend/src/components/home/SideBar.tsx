85d6e9831a2f8fa6674ff8d4f5c48c4a31275c46
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-10-02 15:33:31 +0200

    #10 modificaciones en dashboard para que se pueda navegar correctamente

--- frontend/src/components/home/SideBar.tsx
+++ frontend/src/components/home/SideBar.tsx
@@ -6,38 +6,39 @@ import {
 } from 'lucide-react'
 import {
   Command,
-  CommandGroup,
-  CommandItem
+  CommandGroup
 } from '../ui/command'
+import { Button } from '../ui/button'
 
-export function SideBar () {
+export function SideBar ({ onSidebarItemClick }: { onSidebarItemClick: (component: string) => void }) {
+  console.log('onSidebarItemClick recibido correctamente:', onSidebarItemClick)
   return (
     <div>
-      <div className='space-y-4 py-4'>
+      <div className='py-4'>
         <div className='px-0 py-2'>
-          <div className='space-y-1'>
-            <Command>
-              <p className='text-sm text-muted-foreground mb-2 ml-3'>Hola!</p>
-              <CommandGroup>
-                <CommandItem>
+          <Command>
+            <CommandGroup>
+              <p className='text-sm text-muted-foreground mb-2 ml-2 md:ml-4'>Hola!</p>
+              <div className='flex flex-col'>
+                <Button onClick={() => onSidebarItemClick('resume')} className='border-none bg-transparent hover:bg-transparent text-muted-foreground justify-start hover:text-orange-600'>
                   <LayoutDashboard className='md:mr-2 h-4 w-4 mx-auto md:mx-0' />
-                  <span className='hidden md:flex'>Resumen</span>
-                </CommandItem>
-                <CommandItem>
+                  <span className='hidden md:flex '>Resumen</span>
+                </Button>
+                <Button onClick={() => onSidebarItemClick('calendar')} className='border-none bg-transparent hover:bg-transparent text-muted-foreground justify-start hover:text-orange-600'>
                   <Calendar className='md:mr-2 h-4 w-4 mx-auto md:mx-0' />
-                  <span className='hidden md:flex'>Calendario</span>
-                </CommandItem>
-                <CommandItem>
+                  <span className='w-4 hidden md:flex'>Calendario</span>
+                </Button>
+                <Button onClick={() => onSidebarItemClick('user')} className='border-none bg-transparent hover:bg-transparent text-muted-foreground justify-start hover:text-orange-600'>
                   <Users2 className='md:mr-2 h-4 w-4 mx-auto md:mx-0' />
                   <span className='hidden md:flex'>Usuarios</span>
-                </CommandItem>
-                <CommandItem>
+                </Button>
+                <Button onClick={() => onSidebarItemClick('store')} className='border-none bg-transparent hover:bg-transparent text-muted-foreground justify-start hover:text-orange-600'>
                   <Store className='md:mr-2 h-4 w-4 mx-auto md:mx-0' />
                   <span className='hidden md:flex'>Centros</span>
-                </CommandItem>
-              </CommandGroup>
-            </Command>
-          </div>
+                </Button>
+              </div>
+            </CommandGroup>
+          </Command>
         </div>
       </div>
     </div>
