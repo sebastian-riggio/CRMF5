e95c323947925bba57c8fe21d342a7c56d4fc6d7
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-10-03 15:38:08 +0200

    #18 se traen todas las convocatorias en etapa de cierre. Suponiendo que la etapa de cierre se nombra como cierre

--- frontend/src/services/gestion.ts
+++ frontend/src/services/gestion.ts
@@ -4,6 +4,14 @@ export const getAllGestion = () => {
   return axiosPrivate.get('gestion')
 }
 
+export const getConvocatoriasEnCierre = () => {
+  return axiosPrivate.get('gestion', {
+    params: {
+      'etapaCierre.estado': 'cierre' // corroborar cómo se nombra al estado de la etapa de cierre
+    }
+  })
+}
+
 export const getOneGestion = (id:string) => {
   return axiosPrivate.get(`gestion/${id}`)
 }
