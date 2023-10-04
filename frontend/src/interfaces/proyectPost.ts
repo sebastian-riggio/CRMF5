50d468076069fa23cb43415c96dea213c90a55e9
Acrata <hxtr227@gmail.com>
2023-10-04 22:04:01 +0200

    #24 Form corregido, problema con instancia axios

--- frontend/src/interfaces/proyectPost.ts
+++ frontend/src/interfaces/proyectPost.ts
@@ -3,31 +3,31 @@
 import { z } from 'zod'
 
 const proyectPost = z.object({
-  _id: z.string(),
+  _id: z.string().optional(),
   proyectoNombre: z.string().min(2, {
     message: 'Debe completar este campo'
   }),
 
-  proyectoCodigo: z.string(),
+  proyectoCodigo: z.string().optional(),
 
   centroGestor: z.string().min(2, {
     message: 'Debe completar este campo'
-  }),
+  }).optional(),
   responsable: z.string().min(2, {
     message: 'Debe completar este campo'
-  }),
+  }).optional(),
   proyectoDuracion: z.string().min(1, {
     message: 'Debe completar este campo'
-  }),
+  }).optional(),
   proyectoPresupuesto: z.string().min(2, {
     message: 'Debe completar este campo'
-  }),
+  }).optional(),
   factoriaPresupuesto: z.string().min(2, {
     message: 'Debe completar este campo'
-  }),
+  }).optional(),
 
-  fechaInicio: z.date(),
-  fechaCierre: z.date()
+  fechaInicio: z.date().optional(),
+  fechaCierre: z.date().optional()
 
 })
 
--- frontend/src/services/axiosPrivate.ts
+++ frontend/src/services/axiosPrivate.ts
@@ -4,5 +4,5 @@ const BASE_URL = 'http://localhost:3000/api/v1'
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'multipart/form-data' }
+  // headers: { 'Content-Type': 'multipart/form-data' }
 })
